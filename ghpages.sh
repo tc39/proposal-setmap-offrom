dcc863efe046b1362b1759b6988634ad7af82d88
Leo Balter <leonardo.balter@gmail.com>
2017-05-21 11:56:37 -0400

    Update ghpages script

--- ghpages.sh
+++ ghpages.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -x
 
-npm run build &&
-  git checkout gh-pages &&
+yarn build &&
+  git checkout master &&
   mv out/index.html index.html &&
   mv out/spec.css spec.css &&
   mv out/spec.js spec.js &&
   git add {index.html,spec.css,spec.js} &&
   git commit -a -m "update gh-pages" &&
-  git push origin gh-pages &&
-  git checkout master &&
-open "https://leobalter.github.io/proposal-setmap-offrom"
+  git push origin master &&
+open "https://tc39.github.io/proposal-setmap-offrom"
+
