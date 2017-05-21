ab84ffbae6b049122332b70442c7e1053b0fe423
Leo Balter <leonardo.balter@gmail.com>
2017-05-21 11:59:49 -0400

    partial revert to gh-pages, I don't have admin access to the tc39 fork

--- ghpages.sh
+++ ghpages.sh
@@ -2,12 +2,13 @@
 set -x
 
 yarn build &&
-  git checkout master &&
+  git checkout gh-pages &&
   mv out/index.html index.html &&
   mv out/spec.css spec.css &&
   mv out/spec.js spec.js &&
   git add {index.html,spec.css,spec.js} &&
   git commit -a -m "update gh-pages" &&
-  git push origin master &&
+  git push origin gh-pages &&
+  git checkout master &&
 open "https://tc39.github.io/proposal-setmap-offrom"
 
